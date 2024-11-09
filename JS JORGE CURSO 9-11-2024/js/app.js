2ead5b4ec6e78031236e61190909012cb1c97e32
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-11-09 16:02:26 -0400

    Add files via upload
    clase 9/11/2024

---
+++ JS JORGE CURSO 9-11-2024/js/app.js
@@ -0,0 +1,50 @@
+//==================Practica 1 Aula================================
+// n1 = 13
+// n2 = 15
+// n3 = 90
+
+// if (n3 > n2 && n2 > n1 && n2 < n3) {
+//     console.log("el mayor es n3 con :",n3);  
+// }
+// else if(n2 > n1 && n2 > n1 && n3 > n2 ){
+//     console.log("el mayor es n1 con :",n1);
+// }
+
+// else if(n1 < n2 && n2 > n1 && n3 > n2 ){
+//     console.log("el mayor es n2 con :",n2);
+// }
+// =========================== END =================================
+
+
+
+
+//====================Practica 2 Aula===============================
+
+// n1 = 11;
+// n2 = 29;
+
+// if( n1 < n2 && n2 > n1){
+//     console.log(" el menor de ambos es N2 con un valor de:",n1);
+// }
+// else{
+//     console.log(" N2 es mayor que N1");
+// }
+
+// =========================== END =================================
+
+
+// ====================== Manejo del Dom ===========================
+//Clase 9/11/2024.
+
+document.addEventListener('DOMContentLoaded',() => {
+
+    let variable = document.getElementById("hdi");
+
+    variable.innerText = " Pero lo hice ";
+});
+
+// =========================== END =================================
+
+// ==================== Tarea 2 ejercicios js ======================
+
+// =========================== END =================================
